16aae97b3b71301c956c094f483fa954b21e52c1
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-02-13 13:21:23 -0600

    Ejercicio for con PHP

---
+++ for1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edad</title>
+</head>
+<body>
+<?php
+//imprimir una tabla de multiplicar con for ()
+// luego mejorar para imprimir todas las tablas del 1 al 10 
+for($x = 0;$x < 100; $x++){
+    print(" 〡 ".$x );
+}
+
+echo "<hr>";
+for($i=0 ;$i <10; $i++) {
+    print("<br> 9 x $i = ".(9*$i) );
+}
+echo "<hr>";
+for ($x =1; $x <= 3; $x++)
+?>
+</body>
+</html>
