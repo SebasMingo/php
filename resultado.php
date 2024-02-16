ede5a206daac7c7deac63cea454fd87a4de7e765
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-02-16 16:23:08 -0300

    Add files via upload

--- resultado.php
+++ resultado.php
@@ -13,19 +13,29 @@
     }
 
     // VARIABLE GLOBAL
-    $numero1 = $_GET["num1"];
-    $numero2 = $_GET["num2"];
-    $operacion = $_GET["op"];
+    if(isset($_GET["num1"]) && isset($_GET["num2"]) && isset($_GET["op"])){
+        $numero1 = $_GET["num1"];
+        $numero2 = $_GET["num2"];
+        $operacion = $_GET["op"];
 
-    if($operacion == "suma"){
-        $resultado = sumar($numero1,$numero2);
-    }else if ($operacion == "restar"){
-        $resultado = restar($numero1,$numero2);
-    }else if ($operacion == "multiplicar"){
-        $resultado = multiplicar($numero1,$numero2);
-    }else if ($operacion == "dividir") {
-        $resultado = dividir($numero1,$numero2) ;
-    }   
+        //if($numero1 != "" && $numero2 != "" && $operacion != ""){
+        if(!empty($numero1) && ! empty($numero2) && !empty($operacion)){
+            if($operacion == "suma"){
+                $resultado = sumar($numero1,$numero2);
+            }else if ($operacion == "restar"){
+                $resultado = restar($numero1,$numero2);
+            }else if ($operacion == "multiplicar"){
+                $resultado = multiplicar($numero1,$numero2);
+            }else if ($operacion == "dividir") {
+                $resultado = dividir($numero1,$numero2) ;
+            }
+            echo "El resultado es: ".$resultado;
 
-    echo "El resultado es : ".$resultado;
+
+        }else {
+            echo "Algún campo está vacío";
+        }
+    }else{
+        echo "que haces aqui";
+    }    
 ?>
