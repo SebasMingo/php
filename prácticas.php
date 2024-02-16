6ad14ce10e26380a543ee72b4dc9b21d93ed217b
SebasMingo <124638935+SebasMingo@users.noreply.github.com>
2024-02-16 15:18:50 -0300

    Practicas1

---
+++ prácticas.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+$dia[0]= "Domingo";
+$dia[1]="Lunes";
+$dia[2]= "Martes";
+$dia[3]= "Miercoles";
+$dia[4]= "Jueves";
+$dia[5]= "Viernes";
+$dia[6]= "Sabado";
+
+$hoy = date("N");
+Print ("Hoy es:".$hoy);
